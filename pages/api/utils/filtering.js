2d34281a154c73f371874ef9c0f689d4f0ee5859
Richard <richardkwon5@gmail.com>
2023-05-15 13:13:10 -0700

    finished initial pass for filtering utils

---
+++ pages/api/utils/filtering.js
@@ -0,0 +1,79 @@
+import fs from 'fs'
+import path from 'path'
+import Papa from 'papaparse'
+
+const parseOptions = {
+    dynamicTyping: true,
+    header: true
+}
+
+//parse company data by reading local file and return array of objects
+//each object is a company and contains k, v pair where k is column and v is the value
+
+const parseCompanyData = () => {
+    const csvPath = path.join(process.cwd(), '../', 'data', 'companies.csv');
+    const file = fs.readFileSync(csvPath, { encoding: 'utf-8' })
+    const data = Papa.parse(file, parseOptions).data;
+    return data
+}
+
+//parse company data by reading local file and return array of objects
+//each object is a candidate and contains contains k, v pair where k is column and v is the value
+
+const parseScoreData = () => {
+    const csvPath = path.join(process.cwd(), '../', 'data', 'scores.csv');
+    const file = fs.readFileSync(csvPath, { encoding: 'utf-8' });
+    const data = Papa.parse(file, parseOptions).data;
+    return data
+}
+
+const findCandidateInfo = (candidateId, candidates) => {
+    const candidate = candidates.find(candidate => candidate['candidate_id'] === candidateId)
+    return candidate ? candidate : 'Candidate not found'
+}
+
+const filterCompanyByFractalIndex = (companyId, data) => {
+
+    let targetFractalIdx = null
+    const similarCompanies = {}
+
+    //find fractal index of desired company
+    for (let company of data) {
+        if (company['company_id'] === companyId) {
+            targetFractalIdx = company['fractal_index']
+            break
+        }
+    }
+
+    //filter data and populate similarCompanies object
+    data.forEach(company => {
+        if (Math.abs(company['fractal_index'] - targetFractalIdx) < 0.15) {
+            similarCompanies[company['company_id']] = company['company_id']
+        }
+    })
+    return similarCompanies
+}
+
+const filterScoresByCompanyId = (companies, scores) => {
+    const filteredScores = scores.filter((score) => companies[score['company_id']])
+    return filteredScores
+}
+
+const filterScoresByJobTitle = (candidates, title) =>  {
+    const filteredScores = candidates.filter((candidate) => candidate['title'] == title)
+    return filteredScores
+}
+
+const filteredResults = (candidateId) => {
+    const companies = parseCompanyData()
+    const scores = parseScoreData()
+    const candidate = findCandidateInfo(candidateId, scores)
+    const candidateCompanyId = candidate['company_id']
+    const candidateJobTitle = candidate['title']
+    const similarCompanies = filterCompanyByFractalIndex(candidateCompanyId, companies)
+    const filteredScoresByCompany = filterScoresByCompanyId(similarCompanies, scores)
+    const filteredScoresByJob = filterScoresByJobTitle(filteredScoresByCompany, candidateJobTitle)
+    return filteredScoresByJob
+}
+
+export default filteredResults
